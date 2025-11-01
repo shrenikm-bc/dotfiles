cea88d3f2a178ed0630db903750a8764008dcfe5
shrenikm <shrenik95@gmail.com>
2025-11-01 13:03:24 -0700

    jk now

--- neovim/lvim/lua/config/keymaps.lua
+++ neovim/lvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Save
+map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+-- Save all
+map("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All" })
+-- Quit
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+-- Quit all
+map("n", "<leader>Q", "<cmd>qa<cr>", { desc = "Quit All" })
