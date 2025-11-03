f391ed0037111d416aee76f44c1196688a0e8e2e
shrenikm <shrenik95@gmail.com>
2025-11-02 16:44:40 -0800

    jk now

--- neovim/lvim/lua/config/keymaps.lua
+++ neovim/lvim/lua/config/keymaps.lua
@@ -4,11 +4,9 @@
 
 local map = vim.keymap.set
 
--- Save
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 -- Save all
-map("n", "<leader>W", "<cmd>wa<cr>", { desc = "Save All" })
--- Quit
-map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
--- Quit all
-map("n", "<leader>Q", "<cmd>qa<cr>", { desc = "Quit All" })
+map({ "i", "x", "n", "s" }, "<S-s>", "<cmd>wa<cr><esc>", { desc = "Save All Files" })
+-- Quit window
+map("n", "<leader>qf", "<cmd>q<cr>", { desc = "Quit Window" })
+-- Quit window without saving
+map("n", "<leader>qx", "<cmd>q!<cr>", { desc = "Quit Window" })
