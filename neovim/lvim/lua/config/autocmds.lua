45de246e1e4a2ec0a37cf77e3f8d67b32179e229
shrenikm <shrenik95@gmail.com>
2025-11-15 23:23:58 -0800

    update lvim + tmux + oil + autocmd

--- neovim/lvim/lua/config/autocmds.lua
+++ neovim/lvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable tmux statusline when entering Neovim and re-enable it when leaving
+local group = vim.api.nvim_create_augroup("tmux_status_toggle", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group,
+  callback = function()
+    vim.fn.system("tmux set status off")
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  group = group,
+  callback = function()
+    vim.fn.system("tmux set status on")
+  end,
+})
