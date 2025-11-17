632ce3f4675dd927b41f880997d1c1e1f2dd0cb3
shrenikm <shrenik95@gmail.com>
2025-11-16 20:21:25 -0800

    autocmd debugged

--- neovim/lvim/lua/config/autocmds.lua
+++ neovim/lvim/lua/config/autocmds.lua
@@ -9,16 +9,19 @@
 
 -- Disable tmux statusline when entering Neovim and re-enable it when leaving
 local group = vim.api.nvim_create_augroup("tmux_status_toggle", { clear = true })
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = group,
-  callback = function()
-    vim.fn.system("tmux set status off")
-  end,
-})
 
+-- Because of how LazyVim loads, plugin configs and autocmds are loaded after VimEnter is triggered, so we can't really use this to set up an autocmd for VimEnter.
+-- Instead, to disable the tmux statusline when Neovim starts, we schedule a function.
+vim.schedule(function()
+  if vim.fn.executable("tmux") == 1 then
+    vim.fn.system("tmux set status off")
+  end
+end)
+
+-- Re-enabling is business as usual.
 vim.api.nvim_create_autocmd("VimLeavePre", {
   group = group,
   callback = function()
-    vim.fn.system("tmux set status on")
+    vim.fn.system("tmux set-option status on")
   end,
 })
